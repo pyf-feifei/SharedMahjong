4e10e032f74a6b99b6d7cff3af3bc979fbfb8900
jikeshifu <13886161@qq.com>
2023-07-27 11:26:12 +0800

    提交数据库及缺失文件

--- code/wrzs_apiserver/README.md
+++ code/wrzs_apiserver/README.md
@@ -1,14 +1,17 @@
 ### 硬件配置文件
 
 app\module\hardwareCloud\serverConfig.php
-
-AppId
-
-AppSecret
+默认对接硬件云配置
+AppId 208ac48ff968ab0dc675008d0ded46d1
+AppSecret ad45d97d283c518e15b4bda75c887dec
 
 测试设备
+1.不锈钢wifi门锁     W89601EC6A7
+2.WiFi版门禁控制器   W761790BC7F
+3.空开断路器 W71F9783520
+4.云喇叭 W7022621597
 
-云喇叭 W7022621597
-门锁     W89601EC6A7
-断路器 W71F9783520
+
+购买这些硬件就行了
+
 
--- code/wrzs_apiserver/app/module/hardwareCloud/serverConfig.php
+++ code/wrzs_apiserver/app/module/hardwareCloud/serverConfig.php
@@ -6,9 +6,10 @@ namespace app\module\hardwareCloud;
 
 class serverConfig
 {
+    //默认对接硬件云配置
     static $WiFIUrl = "https://wdev.wmj.com.cn/deviceApi/";
-    static $AppId = "1111111111111111111111111111111111";
-    static $AppSecret = "99999999999999999999999999999999";
+    static $AppId = "208ac48ff968ab0dc675008d0ded46d1";
+    static $AppSecret = "ad45d97d283c518e15b4bda75c887dec";
 
     static function GetUrl(){
         return self::$WiFIUrl;
